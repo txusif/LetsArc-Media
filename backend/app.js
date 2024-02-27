8c8fec4f721559356ccd957bc17a85bf348db09d
Txusif <txusif@gmail.com>
2024-02-27 21:51:35 +0530

    initial frontend/backend

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect(
+  "mongodb+srv://txusif:JjHVgtHgjNnHy5Rk@cluster0.i6jjc1n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+app.get("/backend", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="font-outfit text-gray-400 text-5xl font-black justify-center flex h-screen items-center">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
