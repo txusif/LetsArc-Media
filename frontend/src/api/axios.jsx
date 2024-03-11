cff7892d6d0c973ca2f72e7f7508202e4dc99024
Yash Bhardwaj <yash.bhardwaj5040@gmail.com>
2024-03-11 15:43:12 +0530

    Update axios.jsx

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,8 +1,8 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: "http://localhost:8000/backend",
-  // baseUrl: "https://api.example.com/backend",
+  // baseURL: "http://localhost:8000/backend",
+  baseURL: "https://app.letsarc.com/backend",
 });
 
 export default axios;
